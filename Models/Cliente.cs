1114487d5ff3ae151f2f7c0d5916590a595d3f38
Domingos Ribeiro <domingos.ribeiro@gmail.com>
2021-05-30 19:54:41 +0100

    class cliente

---
+++ Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Trabalho1.Models
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public int Nome { get; set; }
+        public int Email { get; set; }
+        public int Telefone { get; set; }
+
+
+
+    }
+}
