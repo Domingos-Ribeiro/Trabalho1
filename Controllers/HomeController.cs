ab29c45a9eebb8db5ad247a169ae8306734b23b6
Domingos Ribeiro <domingos.ribeiro@gmail.com>
2021-05-30 04:27:16 +0100

    12

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,10 +11,19 @@ namespace Trabalho1.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            ViewData["Nome"] = "Domingos Ribeiro";
-            ViewData["Email"] = "domingos.ribeiro.net@gmail.com";
-            ViewData["Curso"] = "Programação ASP.NET";
-            ViewData["DataInicio"] = new DateTime(2021, 05, 30);
+            // Usando o ViewData (Mais antigo)
+            //ViewData["Nome"] = "Domingos Ribeiro";
+            //ViewData["Email"] = "domingos.ribeiro.net@gmail.com";
+            //ViewData["Curso"] = "Programação ASP.NET";
+            //ViewData["DataInicio"] = new DateTime(2021, 05, 30);
+
+
+            // Usando o ViewBag (Mais recente)
+            ViewBag.Nome = "Domingos Ribeiro";
+            ViewBag.Email = "domingos.ribeiro.net@gmail.com";
+            ViewBag.Curso = "Programação ASP.NET";
+            ViewBag.DataInicio = new DateTime(2021, 05, 30);
+
 
             return View();
         }
