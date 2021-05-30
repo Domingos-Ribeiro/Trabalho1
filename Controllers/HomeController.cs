8e3b27e1bf4c418d5c8797f6f75edb59738da427
Domingos Ribeiro <domingos.ribeiro@gmail.com>
2021-05-30 04:01:09 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Trabalho1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewData["Nome"] = "Domingos Ribeiro";
+            ViewData["Email"] = "domingos.ribeiro.net@gmail.com";
+            ViewData["Curso"] = "Programação ASP.NET";
+            ViewData["DataInicio"] = new DateTime(2021, 05, 30);
+
+            return View();
+        }
+
+   
+    }
+}
